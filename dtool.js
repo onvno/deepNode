8212493ba5b5fac5ec97eb148550f7450ed8f944
onvno <leewei2020@126.com>
2016-10-26 20:32:03 +0800

    add util

---
+++ dtool.js
@@ -0,0 +1,37 @@
+var fs = require('fs');
+var path = require('path');
+
+/**
+ * 筛除'.'开头的隐藏文件
+ * 如'.DS_Store'
+ */
+var regDotFile = function(ele){
+   return /^[^\.]/g.test(ele);
+}
+var rmdot = function(ary){
+   return ary.filter(regDotFile);
+}
+exports.rmdot = rmdot;
+
+ /**
+  * get:当前目录下所有文件夹
+  * http://stackoverflow.com/questions/18112204/get-all-directories-within-directory-nodejs
+  */
+var subdir = function(src) {
+  return fs.readdirSync(src)
+    .filter(function(file){
+      return fs.statSync(path.join(src,file)).isDirectory();
+    })
+}
+exports.subdir = subdir;
+
+/**
+ * get:当前目录下所文件
+ */
+var subfile = function(src) {
+  return fs.readdirSync(src)
+    .filter(function(file){
+      return fs.statSync(path.join(src,file)).isFile();
+    })
+}
+exports.subfile = subfile;
